13d978897c4f2a85eb46dcf9044ae6996ed7cdfc
playtank <yunwenl@gmail.com>
2026-06-30 16:22:44 -0500

    adding examples to test the network flow 06302026

--- feature/login/src/main/java/com/identityx/login/domain/usecase/LoginAndFetchProfileUseCase.kt
+++ feature/login/src/main/java/com/identityx/login/domain/usecase/LoginAndFetchProfileUseCase.kt
@@ -1,4 +1,11 @@
 package com.identityx.login.domain.usecase
 
-class LoginAndFetchProfileUseCase {
+import com.identityx.login.data.LoginRepository
+import javax.inject.Inject
+
+class LoginAndFetchProfileUseCase @Inject constructor(
+    private val repository: LoginRepository
+) {
+    suspend operator fun invoke(email: String, password: String): Result<Unit> =
+        repository.login(email, password)
 }
